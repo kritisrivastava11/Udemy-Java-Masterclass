8008a91df189e9f60dabbd64201e77de1d7c4801
kriti <kriti@localhost>
2021-05-31 14:54:17 +0530

    4th commit

---
+++ FirstLastDigitSum/src/learnJava/Main.java
@@ -0,0 +1,10 @@
+package learnJava;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Sum of first and last digit = " +FirstLastDigitSum.sumFirstAndLastDigit(876));
+
+	}
+
+}
