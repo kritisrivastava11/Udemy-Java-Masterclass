84540b406ac0c62b6b89e3eb1a47e04c414d4079
kriti <kriti@localhost>
2021-05-31 22:20:29 +0530

    All files

---
+++ LastDigitChecker/src/com/kriti/LastDigitChecker.java
@@ -0,0 +1,14 @@
+package com.kriti;
+
+public class LastDigitChecker {
+	public static boolean hasSameLastDigit(int num1 ,int num2, int num3) {
+		if(((num1 < 10) || (num1 > 1000)) || ((num2 < 10) || (num2 > 1000)) || ((num3 < 10) || (num3 > 1000))) {
+			return false;
+		}
+		return (((num1 % 10) == (num2 % 10)) || ((num1 % 10) == (num3 % 10)) || ((num2 % 10) == (num3 % 10)));
+	}
+	public static boolean isValid(int num) {
+		return ((num >= 10) && ( num <= 1000));
+	}
+}
+
