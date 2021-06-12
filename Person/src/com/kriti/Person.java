1daf4d8d8cc9eadba2bad92805061790f6544158
kriti <kriti@localhost>
2021-06-12 11:30:26 +0530

    all files

---
+++ Person/src/com/kriti/Person.java
@@ -0,0 +1,66 @@
+package com.kriti;
+
+public class Person {
+
+	private String firstName;
+	private String lastName;
+	private int age;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setAge(int age) {
+		
+//		this.age  = age;
+//		if(this.age < 0 || this.age > 100) {
+//			this.age = 0;
+//		}
+//
+//		this.age  = age;
+		
+		this.age = (age<0 || age>100) ? 0 : age;
+	}
+	
+	public boolean isTeen() {
+
+//		if(this.age > 12 && this.age < 20) {
+//			return true;
+//		}
+//		return false;
+		
+		return (age>12 && age<20);
+	}
+	
+	public String getFullName() {
+
+//		if(this.firstName.isEmpty() && this.lastName.isEmpty()) {
+//			return "";
+//		}
+//		else if(this.lastName.isEmpty()) {
+//			return this.firstName;
+//		}else if(this.firstName.isEmpty()){
+//			return this.lastName;
+//		}else {
+//			return this.firstName + " " + this.lastName;
+//		}
+		
+		return String.join(" ", firstName, lastName).trim();
+	}
+}
