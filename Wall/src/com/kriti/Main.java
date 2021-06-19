8cd6bacb711cf8a905a1110d8ef7215615d4cd5c
kriti <kriti@localhost>
2021-06-19 18:09:52 +0530

    all files

---
+++ Wall/src/com/kriti/Main.java
@@ -0,0 +1,14 @@
+package com.kriti;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Wall obj1 = new Wall();
+		
+		obj1.setHeight(34);
+		obj1.setWidth(4.6);
+		System.out.println(obj1.getArea());
+
+	}
+
+}
